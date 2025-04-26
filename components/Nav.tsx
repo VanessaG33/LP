ffbd48a053fb4f17b4ee5ba888f2ee9c4a3c40d0
McFly <matthieu.roman2002@gmail.com>
2025-04-25 23:30:29 -0400

    fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
@@ -24,10 +23,11 @@ export default function Nav() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
     <header>
       <nav
-        data-state={menuState && "active"}
+        data-state={menuState ? "active" : "inactive"}
         className="fixed z-20 w-full px-2 top-4"
       >
         <div
@@ -38,6 +38,7 @@ export default function Nav() {
           )}
         >
           <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            {/* Logo + burger */}
             <div className="flex w-full justify-between lg:w-auto">
               <Link
                 href="/"
@@ -49,16 +50,29 @@ export default function Nav() {
 
               <button
                 onClick={() => setMenuState(!menuState)}
-                aria-label={menuState == true ? "Close Menu" : "Open Menu"}
+                aria-label={menuState ? "Close Menu" : "Open Menu"}
                 className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
               >
-                <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
-                <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+                <Menu
+                  className={cn(
+                    "m-auto size-6 duration-200",
+                    "data-[state=active]:rotate-180 data-[state=active]:scale-0 data-[state=active]:opacity-0"
+                  )}
+                  data-state={menuState ? "active" : "inactive"}
+                />
+                <X
+                  className={cn(
+                    "absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200",
+                    "data-[state=active]:rotate-0 data-[state=active]:scale-100 data-[state=active]:opacity-100"
+                  )}
+                  data-state={menuState ? "active" : "inactive"}
+                />
               </button>
             </div>
 
+            {/* Menu desktop */}
             <div className="absolute inset-0 m-auto hidden size-fit lg:block">
-              <ul className="flex gap-8 text-sm text-[#D4D4D4]">
+              <ul className="flex gap-8 text-sm">
                 {menuItems.map((item, index) => (
                   <li key={index}>
                     <Link
@@ -72,9 +86,13 @@ export default function Nav() {
               </ul>
             </div>
 
-            <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+            {/* Menu mobile */}
+            <div
+              data-state={menuState ? "active" : "inactive"}
+              className="bg-background data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none "
+            >
               <div className="lg:hidden">
-                <ul className="space-y-6 text-base text-[#D4D4D4]">
+                <ul className="space-y-6 text-base">
                   {menuItems.map((item, index) => (
                     <li key={index}>
                       <Link
@@ -86,13 +104,6 @@ export default function Nav() {
                     </li>
                   ))}
                 </ul>
-              </div>
-              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
-                <Button asChild variant="contained" size="sm">
-                  <Link href="#call">
-                    <span>Je lance ma conciergerie</span>
-                  </Link>
-                </Button>
               </div>
             </div>
           </div>
