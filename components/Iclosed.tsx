921e6aa03b493169c83a6654d43c702a8a5a3abc
McFly <matthieu.roman2002@gmail.com>
2025-06-23 18:23:30 +0200

    put calendfly

--- components/Iclosed.tsx
+++ components/Iclosed.tsx
@@ -1,40 +1,15 @@
 "use client";
-
-import { useEffect, useRef } from "react";
+import { InlineWidget } from "react-calendly";
 
 export default function IClosed() {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (
-        event.origin === "https://audit-situation-conciergerie.youcanbook.me/"
-      ) {
-        if (iframeRef.current) {
-          iframeRef.current.style.height = `${event.data}px`;
-        }
-      }
-    };
-
-    window.addEventListener("message", handleMessage);
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
-
   return (
-    <div className="relative w-full">
-      <iframe
-        ref={iframeRef}
-        id="ycbm"
-        src="https://audit-situation-conciergerie.youcanbook.me/?embed=true"
-        style={{
-          width: "100%",
-          height: "1000px", // valeur initiale
-          border: "0px",
-          backgroundColor: "transparent",
+    <div className="lg:w-[640px] mx-auto">
+      <InlineWidget
+        pageSettings={{
+          hideEventTypeDetails: true,
+          hideLandingPageDetails: true,
         }}
-        frameBorder="0"
+        url={"https://calendly.com/d/cssq-8vm-hvb/audit-de-situation"}
       />
     </div>
   );
